abdc08aa3111bfc2a072248f7d563c5813477955
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-01-25 02:56:52 -0200

    Refactoring on PlayerEmbed

--- html/js/PlayerEmbed.js
+++ html/js/PlayerEmbed.js
@@ -7,31 +7,17 @@ function PlayerEmbed(parameterNames, parameters) {
 
   var wmodeValue = "direct";
   var wmodeOptions = ["direct", "opaque", "transparent", "window"];
-  if (parameters.hasOwnProperty("wmode"))
-  {
-    if (wmodeOptions.indexOf(parameters.wmode) >= 0)
-    {
+  if (parameters.hasOwnProperty("wmode")) {
+    if (wmodeOptions.indexOf(parameters.wmode) >= 0) {
       wmodeValue = parameters.wmode;
     }
     delete parameters.wmode;
   }
 
-  swfobject.embedSWF(
-    "static/StrobeMediaPlayback.swf"
-    , "BemTVplayer"
-    , 640
-    , 480
-    , "10.1.0"
-    , "expressInstall.swf"
-    , parameters
-    , {
-              allowFullScreen: "true",
-              wmode: wmodeValue
-          }
-    , {
-              name: "BemTVplayer"
-          }
-  );
+  swfobject.embedSWF("static/StrobeMediaPlayback.swf", "BemTVplayer", 640, 360, "10.1.10", "expressInstall.swf",
+      parameters, { allowFullScreen: "true",
+                    wmode: wmodeValue,
+                  }, {name: "BemTVplayer"});
 
 //   function onMediaPlaybackError(playerId, code, message, detail)
 //   {
