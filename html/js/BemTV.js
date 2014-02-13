7e98bc869a7ce2f14d26d8dee85665c61aae94fc
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-02-13 20:59:08 -0200

    making some tests with draft protocol

--- html/js/BemTV.js
+++ html/js/BemTV.js
@@ -17,45 +17,55 @@ BemTV.prototype = {
   _init: function() {
     self = this;
     this.setupPeerConnection();
+    this.chunksCache = {};
+    this.swarmSize = 0;
+    this.bufferedChannel = undefined;
   },
 
   setupPeerConnection: function() {
     console.log("BemTV setup peer connection");
     this.connection = quickconnect(BEMTV_SERVER, {room: 'bemtv', iceServers: ICE_SERVERS});
     this.dataChannel = this.connection.createDataChannel("bemtv");
-    this.dataChannel.on("peer:connect", this.onPeerConnect);
-    this.dataChannel.on("peer:disconnect", this.onPeerDisconnect);
     this.dataChannel.on("bemtv:open", this.onOpen);
+    this.dataChannel.on("peer:connect", this.onConnect);
+    this.dataChannel.on("peer:leave", this.onDisconnect);
   },
 
   onOpen: function(dc, id) {
-    console.log("Chegou gente, mandando string");
-    this.bufferedChannel = buffered(dc);
-    this.bufferedChannel.send("alo");
-    this.bufferedChannel.on('data', function(data) { console.log("pbufferedeer " + id + "says:" + data); });
+    console.log("Peer entered the room: " + id);
+    self.bufferedChannel = buffered(dc);
+    self.bufferedChannel.on('data', function(data) { console.log(id + "says: " + data); });
   },
 
-  onPeerConnect: function(dataChannel, id) {
-    console.log("Peer connected: " + id);
-  },
-
-  onPeerDisconnect: function(dataChannel, id) {
+  onDisconnect: function(id) {
     console.log("Peer disconnected: " + id);
+    self.swarmSize -= 1;
+  },
+
+  onConnect: function(id) {
+    console.log("Peer connected: " + id);
+    self.swarmSize += 1;
   },
 
   requestResource: function(url) {
     console.log("Resource requested by the player: " + url);
+    if (this.swarmSize > 0) {
+      console.log("Swarm have peers, asking if someone have it");
+      this.bufferedChannel.send("chunk:request|" + url);
+    }
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.responseType = 'arraybuffer';
     xhr.onload = this.readBytes;
     xhr.send();
+    this.currentUrl = url;
   },
 
   readBytes: function(e) {
-    console.log("calling readBytes");
+//    console.log("calling readBytes");
     var res = self.base64ArrayBuffer(e.currentTarget.response);
     var bemtvPlayer = document.getElementById('BemTVplayer');
+    self.chunksCache[self.currentUrl] = res;
     bemtvPlayer.resourceLoaded(res);
   },
 
@@ -96,40 +106,3 @@ BemTV.prototype = {
 }
 
 module.exports = BemTV;
-//module.exports = quickconnect;
-//module.exports = buffered;
-
-//var quickconnect = require('rtc-quickconnect');
-//var buffered = require('rtc-bufferedchannel');
-//
-//var connection = quickconnect('http://server.bem.tv:8080', { room: 'buffer', debug: true,
-//                                iceServers: [
-//                                     {url: 'stun:stun.l.google.com:19302'},
-//                                     {url:"turn:numb.viagenie.ca:3478", username: "flavio@bem.tv", credential: "bemtvpublic"}
-//                                            ] });
-//
-//var datachannel = connection.createDataChannel("bemtv");
-//
-////connection.on('peer', function(pc, id, data, monitor) {
-////    console.log('got a new friend, id: ' + id, pc);
-////});
-//
-//datachannel.on("bemtv:open", function(dc, id) {
-//        console.log('test dc open for peer: ' + id);
-//        bufferedDc = buffered(dc);
-//        //console.log(bufferedDc);
-//        bufferedDc.send("sim!");
-//        bufferedDc.onmessage = function(evt) { console.log("pbufferedeer " + id + "says:" + evt.data); };
-//        bufferedDc.on('data', function(data) {
-//                doc = document.getElementById("ae");
-//                doc.innerHTML = "rolou ->" + data; });
-//});
-//
-//datachannel.on('peer:connect', function(pc, id, data) {
-//        console.log("Chegou um doidao novo: " + id);
-//});
-//
-//datachannel.on('peer:disconnect', function(pc, id, data) {
-//        console.log("Vazou um doidao: " + id);
-//});
-//
